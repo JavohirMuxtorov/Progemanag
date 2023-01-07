8675829fba48bd1b699fba7bca45dae3c23675a2
javohir <https://github.com/JavohirMuxtorov/Progemanag.git>
2023-01-07 14:39:04 +0300

    commit2

--- app/src/main/java/com/example/trelloclone/activity/TaskListActivity.kt
+++ app/src/main/java/com/example/trelloclone/activity/TaskListActivity.kt
@@ -2,9 +2,12 @@ package com.example.trelloclone.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trelloclone.R
+import com.example.trelloclone.adapters.TaskListItemAdapter
 import com.example.trelloclone.firebase.FirestoreClass
 import com.example.trelloclone.models.Board
+import com.example.trelloclone.models.Task
 import com.example.trelloclone.utils.Constants
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import kotlinx.android.synthetic.main.activity_task_list.*
@@ -35,5 +38,13 @@ class TaskListActivity : BaseActivity() {
     fun boardDetails(board: Board){
         hideProgressDialog()
         setupActionBar(board.name)
+        val addTaskList = Task(resources.getString(R.string.add_list))
+        board.taskList.add(addTaskList)
+
+        rv_task_list.layoutManager = LinearLayoutManager(
+            this, LinearLayoutManager.HORIZONTAL,false)
+        rv_task_list.setHasFixedSize(true)
+        val adapter = TaskListItemAdapter(this,board.taskList)
+        rv_task_list.adapter = adapter
     }
 }
